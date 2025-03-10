f05e151ee776153d6be721f4c62fb7f4ea0f0910
Michael Armijo <marmijo1@uci.edu>
2025-03-09 23:41:47 -0700

    added episodes prototype

--- earth-3.0/src/Episodes.tsx
+++ earth-3.0/src/Episodes.tsx
@@ -1,13 +1,55 @@
+import { useState } from "react"; 
 import { Link } from "react-router-dom"; 
 import "./css/Episodes.css"; 
 
+const episodes = [
+    { id: 1, title: "Episode 1", description: "Description for Episode 1", charities: "Charity 1", image: "screenshot.png"}, 
+    { id: 2, title: "Episode 2", description: "Description for Episode 2", charities: "Charity 2", image: "screenshot.png"}, 
+    { id: 3, title: "Episode 3", description: "Description for Episode 3", charities: "Charity 3", image: "screenshot.png"}, 
+]
 const Episodes = () => { 
+    const [currentIndex, setCurrentIndex] = useState(0); 
+    
+    const nextEpisode = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1 ) % episodes.length); 
+    }; 
+
+    const prevEpisode = () => {
+        setCurrentIndex((prevIndex) => prevIndex > 0 ? prevIndex - 1 : episodes.length - 1);
+    };
+
     return (
         <div className="episodes-page"> 
-            <h1>Episodes of Earth 3.0</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, soluta accusantium! Et error earum, voluptatibus laboriosam iure commodi consequatur doloremque neque voluptatem maiores, blanditiis ipsam, a cupiditate. Laborum, voluptates nulla?</p>
+           <Link to = "/" className="back-button"> ⬅ Travel Back </Link>
 
-            <Link to= "/" className="back-button">Back to Home</Link>
+           <h1 className="episodes-title">EPISODES</h1>
+
+           <div className="carousel">
+                <button className="arrow left" onClick={prevEpisode}>⬅</button>
+                <div className="episode-card">
+                    <h2>{episodes[currentIndex].image} </h2>
+                    <div className="episode-image"> 
+                        <img src={episodes[currentIndex].image} alt={episodes[currentIndex].title} />
+                    </div>
+                </div>
+                <button className="arrow right" onClick={nextEpisode}>⮕</button>
+           </div>
+
+            <div className="dots">
+                {episodes.map((_, index) => (
+                    <span key={index} className={`dot ${index === currentIndex ? "active" : ""}`} />
+                ))}
+            </div>
+
+            <div className="episode-details">
+                <h3 className="detail-title">Description:</h3>
+                <p>{episodes[currentIndex].description}</p>
+
+                <h3 className="detail-title">Charities:</h3>
+                <p>{episodes[currentIndex].charities}</p>
+
+            </div>
+
         </div>
     );
 };
