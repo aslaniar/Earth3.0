ddffd409bdc8c8b07a9e67b312312becd4365ac4
Michael Armijo <marmijo1@uci.edu>
2025-04-15 22:35:21 -0700

    Styling and media for episode tab

--- earth-3.0/src/Episodes.tsx
+++ earth-3.0/src/Episodes.tsx
@@ -3,9 +3,27 @@ import { Link } from "react-router-dom";
 import "./css/Episodes.css"; 
 
 const episodes = [
-    { id: 1, title: "Episode 1", description: "Description for Episode 1", charities: "Charity 1", image: "screenshot.png"}, 
-    { id: 2, title: "Episode 2", description: "Description for Episode 2", charities: "Charity 2", image: "screenshot.png"}, 
-    { id: 3, title: "Episode 3", description: "Description for Episode 3", charities: "Charity 3", image: "screenshot.png"}, 
+    {
+        id: 1, 
+        title: "Episode 1", 
+        description: "On what seems like just another birthday, Dawn—an unassuming woman with a spark for sarcasm and a love for her cat—is unexpectedly thrust into a mysterious, cosmic adventure. Guided by an eccentric team of higher beings and AI, she’s asked to help shape the future of a struggling Earth. Blending humor, heart, and sci-fi charm, the episode explores what happens when an ordinary person is given an extraordinary role in reimagining humanity’s next chapter—whether she wants to or not.", 
+        charities: "https://www.catf.us /https://gfi.org/", 
+        image: "/Episode1.png"
+    }, 
+    { 
+        id: 2,
+        title: "Episode 2",
+        description: "Description for Episode 2",
+        charities: "Charity 2", 
+        image: "screenshot.png"
+    }, 
+    { 
+        id: 3, 
+        title: "Episode 3", 
+        description: "Description for Episode 3", 
+        charities: "Charity 3", 
+        image: "screenshot.png"
+    }, 
 ]
 const Episodes = () => { 
     const [currentIndex, setCurrentIndex] = useState(0); 
@@ -26,12 +44,14 @@ const Episodes = () => {
 
            <div className="carousel">
                 <button className="arrow left" onClick={prevEpisode}>⬅</button>
-                <div className="episode-card">
-                    <h2>{episodes[currentIndex].image} </h2>
-                    <div className="episode-image"> 
-                        <img src={episodes[currentIndex].image} alt={episodes[currentIndex].title} />
+                    <div className="episode-container">
+                        <h2>{episodes[currentIndex].title} </h2>
+                        <div className="episode-card">
+                            <div className="episode-image">
+                                <img src={episodes[currentIndex].image} alt={episodes[currentIndex].title} />
+                            </div> 
+                        </div>
                     </div>
-                </div>
                 <button className="arrow right" onClick={nextEpisode}>⮕</button>
            </div>
 
