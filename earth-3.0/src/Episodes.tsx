b77baa49fde82af74d6fcf1a69f8c6d4fb22f898
Michael Armijo <marmijo1@uci.edu>
2025-04-24 23:26:32 -0700

    Fixed images not popping up and styling errors

--- earth-3.0/src/Episodes.tsx
+++ earth-3.0/src/Episodes.tsx
@@ -1,30 +1,9 @@
 import { useState } from "react"; 
 import { Link } from "react-router-dom"; 
-import "./css/Episodes.css"; 
+import "./css/Episodes.css";
+import { episodes } from "./EpisodeData";
 
-const episodes = [
-    {
-        id: 1, 
-        title: "Episode 1", 
-        description: "On what seems like just another birthday, Dawn—an unassuming woman with a spark for sarcasm and a love for her cat—is unexpectedly thrust into a mysterious, cosmic adventure. Guided by an eccentric team of higher beings and AI, she’s asked to help shape the future of a struggling Earth. Blending humor, heart, and sci-fi charm, the episode explores what happens when an ordinary person is given an extraordinary role in reimagining humanity’s next chapter—whether she wants to or not.", 
-        charities: "https://www.catf.us /https://gfi.org/", 
-        image: "/Episode1.png"
-    }, 
-    { 
-        id: 2,
-        title: "Episode 2",
-        description: "Description for Episode 2",
-        charities: "Charity 2", 
-        image: "screenshot.png"
-    }, 
-    { 
-        id: 3, 
-        title: "Episode 3", 
-        description: "Description for Episode 3", 
-        charities: "Charity 3", 
-        image: "screenshot.png"
-    }, 
-]
+
 const Episodes = () => { 
     const [currentIndex, setCurrentIndex] = useState(0); 
     
