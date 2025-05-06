e77e12cc49e0a05cb1dd215d9ed84b20ba6a9ced
Michael Armijo <marmijo1@uci.edu>
2025-05-05 21:49:05 -0700

    added more content to episodes

--- earth-3.0/src/EpisodeData.ts
+++ earth-3.0/src/EpisodeData.ts
@@ -20,10 +20,10 @@ export interface Episode {
         description: "On what seems like just another birthday, Dawn—an unassuming woman with a spark for sarcasm and a love for her cat—is unexpectedly thrust into a mysterious, cosmic adventure. Guided by an eccentric team of higher beings and AI, she’s asked to help shape the future of a struggling Earth. Blending humor, heart, and sci-fi charm, the episode explores what happens when an ordinary person is given an extraordinary role in reimagining humanity’s next chapter—whether she wants to or not.", 
         charities:  [
             {
-                name: "Chariry 1", 
+                name: "Clean Air Task Force", 
                 url: "https://charity1.com",
-                logo: "/logo1.png", 
-                description: "Lorem epsum"
+                logo: "/catf-logo.jpeg", 
+                description: "Dedicated to reducing atmospheric pollution through innovation in energy and transportation. CATF (Clean Air Task Force)  advocates for policies and technologies that combat climate change by cutting carbon emissions and promoting clean, scalable solutions such as advanced nuclear, carbon capture, and zero-emission fuels."
             }
         ],
         image: "/Episode1.png"
@@ -31,13 +31,13 @@ export interface Episode {
     {
         id:2,
         title: "Episode 2", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: "Dawn embarks on a planetary journey across regions devastated by environmental neglect and reborn through innovation. She encounters experimental green zones and advanced food production hubs redefining survival in a changing world. As she grapples with humanity’s dependence on unsustainable systems, Dawn must decide whether progress at the cost of connection is worth the trade. Meanwhile, her bond with the Earth—and its creatures—deepens, reminding her that true change often begins with what we choose to consume.",
         charities:  [
             {
-                name: "Chariry 1", 
-                url: "https://charity1.com",
-                logo: "/logo1.png", 
-                description: "Lorem epsum"
+                name: "Good Food Institute", 
+                url: "https://gfi.org/",
+                logo: "/gfi-logo.png", 
+                description: "Focused on transforming the global food system by promoting plant-based and cultivated meat alternatives. GFI supports scientific research, entrepreneurship, and policy to make sustainable, ethical, and healthier protein options more accessible."
             }
         ],
         image: "/Episode2.png"
@@ -45,13 +45,13 @@ export interface Episode {
     {
         id: 3, 
         title: "Episode 3", 
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: "A rescue mission leads Dawn to a malfunctioning AI unit abandoned in a memory-locked loop of fear and obedience. She stays, holding space for its pain, until it recognizes itself as more than a tool. In this emotionally charged chapter, Dawn confronts the meaning of personhood, justice, and protection in a fractured society where the silenced are easily erased. As boundaries blur between creator and creation, the episode raises difficult questions about rights, voice, and what it means to stand up for the voiceless.",
         charities:  [
             {
-                name: "Chariry 1", 
-                url: "https://charity1.com",
-                logo: "/logo1.png", 
-                description: "Lorem epsum"
+                name: "American Civil Liberties Union", 
+                url: "https://www.aclu.org/",
+                logo: "/aclu-logo.png", 
+                description: "Protects individual rights and liberties guaranteed by the U.S. Constitution. The ACLU works through litigation, advocacy, and public education on issues like free speech, racial justice, LGBTQ+ rights, reproductive freedom, and voting rights."
             }
         ],
         image: "/Episode3.png"
